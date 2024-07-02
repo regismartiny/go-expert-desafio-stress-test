3059525d80304334fc55716a14f7924e5899bf5d
Régis Martiny <regismartiny@gmail.com>
2024-07-02 16:03:18 -0300

    Command and flags ok

--- cmd/run.go
+++ cmd/run.go
@@ -13,9 +13,14 @@ import (
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run the stress test",
-	Long:  `Run the stress test providing the web service URL`,
+	Long:  `Run the stress test providing the web service URL, the total number of requests and the number of concurrent calls`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+
+		flagUrl, _ := cmd.Flags().GetString("url")
+		flagRequests, _ := cmd.Flags().GetInt("requests")
+		flagConcurrency, _ := cmd.Flags().GetInt("concurrency")
+
+		runStressTest(flagUrl, flagRequests, flagConcurrency)
 	},
 }
 
@@ -26,11 +31,15 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	runCmd.PersistentFlags().String("url", "", "URL of the service")
-	runCmd.PersistentFlags().Int("requests", 10, "Total number of requests")
-	runCmd.PersistentFlags().Int("concurrency", 10, "Number of simultaneous calls")
+	runCmd.PersistentFlags().StringP("url", "u", "", "URL of the service")
+	runCmd.PersistentFlags().IntP("requests", "r", 10, "Total number of requests")
+	runCmd.PersistentFlags().IntP("concurrency", "c", 10, "Number of simultaneous calls")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func runStressTest(url string, requests int, concurrency int) {
+	fmt.Printf("Running stresst test for url: %s, with %d total requests and %d concurrent calls\n", url, requests, concurrency)
 }
