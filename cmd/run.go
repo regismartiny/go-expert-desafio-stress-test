cdcb54936705c75c66db535d0540b7f94785b7cb
Régis Martiny <regismartiny@gmail.com>
2024-07-02 15:50:39 -0300

    Cobra CLI added

---
+++ cmd/run.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run the stress test",
+	Long:  `Run the stress test providing the web service URL`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("run called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	runCmd.PersistentFlags().String("url", "", "URL of the service")
+	runCmd.PersistentFlags().Int("requests", 10, "Total number of requests")
+	runCmd.PersistentFlags().Int("concurrency", 10, "Number of simultaneous calls")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/regismartiny/go-expert-desafio-stress-test/cmd"
+
+func main() {
+	cmd.Execute()
+}
